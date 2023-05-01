f0f56ea0b2532864780821e16f2548183a46070a
joaoCS <joaoantoniogba@hotmail.com>
2023-05-01 16:20:34 -0300

    feat: rota login

--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,14 @@ const router = express.Router();
 
 import { UserModel } from "../models/Users.js";
 
+const JWT_SECRET = process.env.SECRET;
+
+
 export const verifyToken = (req, res, next) => {
     const token = req.headers.authorization;
 
     if (token) {
-        jwt.verify(token, "secret", (err) => {
+        jwt.verify(token, JWT_SECRET, (err) => {
             if (err) {
                 res.sendStatus(403);
                 next();
@@ -46,6 +49,26 @@ router.post("/createAdmin", async (req, res) => {
         res.status(500);
         res.json("Erro ao criar usuário!");
     }
+});
+
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await UserModel.findOne({ email });
+
+    if(!user) {
+        res.status(401);
+        return res.json({ message: "Email não cadastrado!" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    
+    if (!isPasswordValid) {
+        res.status(401);
+        return res.json({ message: "Senha incorreta!" });
+    }
+
+    const token = jwt.sign({  });
 });
 
 export { router as userRouter };
